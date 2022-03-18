57e0fd93588d75a88c2e3845b8cceab3d91f4ddd
dstirjan <stirjan.dejan@gmail.com>
2022-03-18 19:10:28 +0100

    First try colaboration

---
+++ Test/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Doctor : Person
+    {
+        public string Field { get; set; }
+        public string OIB { get; set; }
+
+
+    }
+}
---
+++ Test/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    
+    abstract public class Person 
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public Address Address { get; set; }
+        public virtual string GetAddress()
+        {
+            return $"Address: {Address.Street} {Address.StreetNumber} {Address.City}";
+        }
+    }
+}
